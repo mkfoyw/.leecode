8dcaf13c6724dd9da9fa3249112365f62cbede4b
mkfoyw <mkfoyw@gmail.com>
2021-08-17 16:48:52 +0800

    2021年 8月17日 星期二 16时48分52秒 CST

--- test/main_test.go
+++ test/main_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestLongestPalindrome(t *testing.T) {
-	t1 := []int{0, 1, 0, 1}
+	t1 := []int{2, 1, 5, 6, 2, 3}
 	res := largestRectangleArea(t1)
 	t.Log(res)
 
--- test/other.go
+++ test/other.go
@@ -1,5 +1,25 @@
 package main
 
+/*
+ * @lc app=leetcode.cn id=84 lang=golang
+ *
+ * [84] 柱状图中最大的矩形
+ */
+// 暴力算法： 枚举可能矩形的高度或者高度可以暴力得出结果。
+//
+
+// @lc code=start
+
+/*
+ * @lc app=leetcode.cn id=84 lang=golang
+ *
+ * [84] 柱状图中最大的矩形
+ */
+// 暴力算法： 枚举可能矩形的高度或者高度可以暴力得出结果。
+//
+
+// @lc code=start
+
 func largestRectangleArea(heights []int) int {
 	n := len(heights)
 	if n == 0 {
@@ -17,77 +37,42 @@ func largestRectangleArea(heights []int) int {
 	left := make([]int, n, n)
 	stack := make([]int, 0)
 	for i := 0; i < n; i++ {
+		for len(stack) > 0 && heights[stack[len(stack)-1]] >= heights[i] {
+			stack = stack[:len(stack)-1]
+		}
 
 		if len(stack) == 0 {
-			left[i] = i
-			stack = append(stack, i)
-			continue
-		}
-
-		cur := len(stack) - 1
-		for cur >= 0 {
-			if heights[stack[cur]] <= heights[i] {
-				break
-			}
-			cur--
-		}
-
-		if cur < 0 {
-			left[i] = stack[0]
-			stack = stack[:0]
-			stack = append(stack, i)
+			left[i] = -1
 		} else {
-			if heights[stack[cur]] == heights[i] {
-				left[i] = stack[cur]
-				stack = stack[:cur+1]
-			} else {
-				left[i] = stack[cur] + 1
-				stack = stack[:cur+1]
-				stack = append(stack, i)
-			}
+			left[i] = stack[len(stack)-1]
 		}
+
+		stack = append(stack, i)
 	}
 
-	//找到最右边到 i 之间矩形高度都大于heights[i]的矩形编号
 	right := make([]int, n, n)
 	stack = stack[:0]
-	for i := n - 1; i >= 0; i-- {
+	for i := n - 1; i >= 0; i++ {
+		for len(stack) > 0 && heights[stack[len(stack)-1]] >= heights[i] {
+			stack = stack[:len(stack)-1]
+		}
+
 		if len(stack) == 0 {
-			right[i] = i
-			stack = append(stack, i)
-			continue
-		}
-
-		cur := len(stack) - 1
-		for cur >= 0 {
-			if heights[stack[cur]] <= heights[i] {
-				break
-			}
-			cur--
-		}
-
-		if cur < 0 {
-			right[i] = stack[0]
-			stack = stack[:0]
-			stack = append(stack, i)
-
+			right[i] = n
 		} else {
-			if heights[stack[cur]] == heights[i] {
-				right[i] = stack[cur]
-				stack = stack[:cur+1]
-			} else {
-				right[i] = stack[cur] - 1
-				stack = stack[:cur+1]
-				stack = append(stack, i)
-
-			}
+			right[i] = stack[len(stack)-1]
 		}
+		stack = append(stack, i)
 	}
 
 	res := 0
 	for i := 0; i < n; i++ {
-		res = max(res, (right[i]-left[i]+1)*heights[i])
+		res = max(res, (right[i]-left[i]-1)*heights[i])
 	}
 
 	return res
 }
+
+// @lc code=end
+
+// @lc code=end
